18a69205f42938dc929238debeda542a68e07577
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2024-03-10 22:14:09 -0400

    Fix issue

--- Data/DbInitializer/DbInitializer.cs
+++ Data/DbInitializer/DbInitializer.cs
@@ -31,21 +31,22 @@ namespace ClearEdge_Tables.Data.DbInitializer
             {
                 _roleManager.CreateAsync(new IdentityRole("User")).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole("Admin")).GetAwaiter().GetResult();
-            }
-            _userManager.CreateAsync(new Customer
-            {
-                UserName = "admin@example.com",
-                Email = "admin@example.com",
-                Name = "Admin",
-                PhoneNumber = "+1 (226) 724 7739",
-                StreetAddress = "2741, Sierra Drive",
-                State = "Ontario",
-                PostalCode = "23422",
-                City = "Windsor"
-            }, "Secret123*").GetAwaiter().GetResult();
 
-            Customer user = _context.Customer.FirstOrDefault(c => c.Email == "admin@example.com");
-             _userManager.AddToRoleAsync(user, "Admin").GetAwaiter().GetResult();
+                _userManager.CreateAsync(new Customer
+                {
+                    UserName = "admin@example.com",
+                    Email = "admin@example.com",
+                    Name = "Admin",
+                    PhoneNumber = "+1 (226) 724 7739",
+                    StreetAddress = "2741, Sierra Drive",
+                    State = "Ontario",
+                    PostalCode = "23422",
+                    City = "Windsor"
+                }, "Secret123*").GetAwaiter().GetResult();
+
+                Customer user = _context.Customer.FirstOrDefault(c => c.Email == "admin@example.com");
+                 _userManager.AddToRoleAsync(user, "Admin").GetAwaiter().GetResult();
+            }
 
             return;
         }
