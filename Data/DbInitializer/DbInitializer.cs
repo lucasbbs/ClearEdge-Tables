5b6482e5c01e8ec34f224e8aed6611c8ef3ba912
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2024-03-13 21:28:39 -0400

    Comment out piece of code from DbInitialize

--- Data/DbInitializer/DbInitializer.cs
+++ Data/DbInitializer/DbInitializer.cs
@@ -27,26 +27,26 @@ namespace ClearEdge_Tables.Data.DbInitializer
             }catch (Exception ex)
             {}
 
-            if (!_roleManager.RoleExistsAsync("User").GetAwaiter().GetResult() )
-            {
-                _roleManager.CreateAsync(new IdentityRole("User")).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole("Admin")).GetAwaiter().GetResult();
+            //if (!_roleManager.RoleExistsAsync("User").GetAwaiter().GetResult() )
+            //{
+            //    _roleManager.CreateAsync(new IdentityRole("User")).GetAwaiter().GetResult();
+            //    _roleManager.CreateAsync(new IdentityRole("Admin")).GetAwaiter().GetResult();
 
-                _userManager.CreateAsync(new Customer
-                {
-                    UserName = "admin@example.com",
-                    Email = "admin@example.com",
-                    Name = "Admin",
-                    PhoneNumber = "+1 (226) 724 7739",
-                    StreetAddress = "2741, Sierra Drive",
-                    State = "Ontario",
-                    PostalCode = "23422",
-                    City = "Windsor"
-                }, "Secret123*").GetAwaiter().GetResult();
+            //    _userManager.CreateAsync(new Customer
+            //    {
+            //        UserName = "admin@example.com",
+            //        Email = "admin@example.com",
+            //        Name = "Admin",
+            //        PhoneNumber = "+1 (226) 724 7739",
+            //        StreetAddress = "2741, Sierra Drive",
+            //        State = "Ontario",
+            //        PostalCode = "23422",
+            //        City = "Windsor"
+            //    }, "Secret123*").GetAwaiter().GetResult();
 
-                Customer user = _context.Customer.FirstOrDefault(c => c.Email == "admin@example.com");
-                 _userManager.AddToRoleAsync(user, "Admin").GetAwaiter().GetResult();
-            }
+            //    Customer user = _context.Customer.FirstOrDefault(c => c.Email == "admin@example.com");
+            //     _userManager.AddToRoleAsync(user, "Admin").GetAwaiter().GetResult();
+            //}
 
             return;
         }
