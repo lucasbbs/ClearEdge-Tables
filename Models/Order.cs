5d4e27c52f754231371910809a55342382382605
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2024-03-16 16:23:22 -0400

    Add two new fields on the Order model UpdateStatus and UpdateStripePaymentId

--- Models/Order.cs
+++ Models/Order.cs
@@ -24,6 +24,9 @@ namespace ClearEdge_Tables.Models
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+
         [Required]
         public string PhoneNumber { get; set; }
         [Required]
