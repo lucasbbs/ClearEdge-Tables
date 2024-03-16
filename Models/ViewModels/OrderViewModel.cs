2df15dac201b28ae8c00a0ebd4a1dde21e320344
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2024-03-16 16:38:14 -0400

    Create a view model for orders

---
+++ Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ClearEdge_Tables.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public Order Order { get; set; }
+        public IEnumerable<OrderItem> OrderItem { get; set; }
+
+    }
+}
