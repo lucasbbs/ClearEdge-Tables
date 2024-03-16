0ca934203ed8768fbb5cc07c7e9b730d0dfe5637
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2024-03-16 16:16:34 -0400

    - Create new Order Confirmation screen - Provide new feature Order Item - fixes

--- Areas/Customer/Controllers/ShoppingCartController.cs
+++ Areas/Customer/Controllers/ShoppingCartController.cs
@@ -3,6 +3,7 @@ using ClearEdge_Tables.Models.ViewModels;
 using ClearEdge_Tables.Repository.IRepository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace ClearEdge_Tables.Areas.Customer.Controllers
@@ -51,16 +52,14 @@ namespace ClearEdge_Tables.Areas.Customer.Controllers
                 Order = new()
             };
 
-            /*           
             ShoppingCartViewModel.Order.Customer = _unitOfWork.Customer.Get(c => c.Id == userId);
-
             ShoppingCartViewModel.Order.Name = ShoppingCartViewModel.Order.Customer.UserName;
             ShoppingCartViewModel.Order.PhoneNumber = ShoppingCartViewModel.Order.Customer.PhoneNumber;
             ShoppingCartViewModel.Order.StreetAddress = ShoppingCartViewModel.Order.Customer.StreetAddress;
             ShoppingCartViewModel.Order.City = ShoppingCartViewModel.Order.Customer.City;
             ShoppingCartViewModel.Order.State = ShoppingCartViewModel.Order.Customer.State;
             ShoppingCartViewModel.Order.PostalCode = ShoppingCartViewModel.Order.Customer.PostalCode;
-            */
+
             foreach (var shoppingCart in ShoppingCartViewModel.ShoppingCartList)
             {
 
@@ -96,9 +95,75 @@ namespace ClearEdge_Tables.Areas.Customer.Controllers
 				HttpContext.Session.SetInt32("SessionShoppingCart",
                 _unitOfWork.ShoppingCart.GetAll(u => u.CustomerId == userId).Count());
 				_unitOfWork.Save();
-				return RedirectToAction("Index");
+
+                foreach (var shoppingCart in ShoppingCartViewModel.ShoppingCartList)
+                {
+                    OrderItem orderItem = new() {
+                        TableId = shoppingCart.TableId,
+                        OrderId = ShoppingCartViewModel.Order.Id,
+                        Price = shoppingCart.Price,
+                        Count = shoppingCart.Count,
+                    };
+                    _unitOfWork.OrderItem.Add(orderItem);
+                    _unitOfWork.Save();
+                }
+                var domain = Request.Scheme + "://" + Request.Host;
+				var options = new SessionCreateOptions
+                {
+                    SuccessUrl = domain + "/Customer/ShoppingCart/OrderConfirmation/"+ShoppingCartViewModel.Order.Id,
+                    CancelUrl = domain + "/Customer/ShoppingCart/Index",
+                    LineItems = new List<SessionLineItemOptions>(),
+                    Mode = "payment",
+                };
+                foreach (var item in ShoppingCartViewModel.ShoppingCartList)
+                {
+                    var sessionLineItem = new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long)(item.Price * 100),
+                            Currency = "cad",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = item.Table.Name,
+                            }
+                        },
+                        Quantity = item.Count
+                    };
+                    options.LineItems.Add(sessionLineItem);
+                }
+                var service = new SessionService();
+                Session session = service.Create(options);
+
+                _unitOfWork.Order.UpdateStripePaymentId(ShoppingCartViewModel.Order.Id, session.Id, session.PaymentIntentId);
+                _unitOfWork.Save();
+
+                Response.Headers.Add("Location", session.Url);
+                return new StatusCodeResult(303);
             }
             return View(ShoppingCartViewModel);
+        }
+
+        public IActionResult OrderConfirmation(int id) {
+
+            Order orderHeader = _unitOfWork.Order.Get(u => u.Id == id, includeProperties: "Customer");
+
+            var service = new SessionService();
+            Session session = service.Get(orderHeader.SessionId);
+
+            if (session.PaymentStatus.ToLower() == "paid")
+            {
+                _unitOfWork.Order.UpdateStripePaymentId(id, session.Id, session.PaymentIntentId);
+                _unitOfWork.Order.UpdateStatus(id, "Approved", "Approved");
+                _unitOfWork.Save();
+            }
+
+            List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
+                .GetAll(u => u.CustomerId == orderHeader.customerId).ToList();
+
+            _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+            _unitOfWork.Save();
+            return View(id); 
         }
         public IActionResult Plus(int shoppingCartId)
         {
